b747e672e1cbc2b9180875ac5971a43367aa73a2
ivencomur <ivancortes@hotmail.com>
2025-05-14 19:07:20 -0400

    3.5 wip

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://iecm-movies-app-6966360ed90e.herokuapp.com/login.html", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Login failed, please verify your credentials");
+          throw new Error("Login failed");
+        }
+      })
+      .then((data) => {
+        console.log("login successful, API responding:", data);
+        if (onLoggedIn) {
+          onLoggedIn(data.user, data.token);
+        }
+      })
+      .catch((e) => {
+        console.error("login error", e);
+      });
+  };
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="7"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
